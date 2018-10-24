7231146a519ae894a707a276f9a69c962e5bce35
Jorgen <905095514@qq.com>
2018-10-24 17:33:45 +0800

    login.vue page

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
 
 Vue.use(Router)
+
+//require.ensure( dependencies[], callback(require), chunkName: String)按需加载函数
+const login = r => require.ensure([], () => r(require('@/page/login')), 'login');
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'login',
+      component: login
     }
   ]
 })
