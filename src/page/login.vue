958eaaa33ebe924a6d50f427660c04b2cd45dacb
Jorgen <905095514@qq.com>
2018-10-26 17:55:14 +0800

    删除原有js

--- src/page/login.vue
+++ src/page/login.vue
@@ -3,11 +3,11 @@
 	  	<transition name="form-fade" mode="in-out">
 	  		<section class="form_contianer" v-show="showLogin">
 		  		<div class="manage_tip">
-		  			<p>elm后台管理系统</p>
+		  			<p>后台管理系统</p>
 		  		</div>
 		    	<el-form :model="loginForm" :rules="rules" ref="loginForm">
 					<el-form-item prop="username">
-						<el-input v-model="loginForm.username" placeholder="用户名"><span>dsfsf</span></el-input>
+						<el-input v-model="loginForm.username" placeholder="用户名"></el-input>
 					</el-form-item>
 					<el-form-item prop="password">
 						<el-input type="password" placeholder="密码" v-model="loginForm.password"></el-input>
@@ -17,8 +17,7 @@
 				  	</el-form-item>
 				</el-form>
 				<p class="tip">温馨提示：</p>
-				<p class="tip">未登录过的新用户，自动注册</p>
-				<p class="tip">注册过的用户可凭账号密码登录</p>
+				<p class="tip">并没有温馨提示</p>
 	  		</section>
 	  	</transition>
   	</div>
@@ -47,9 +46,12 @@
 			}
 		},
 		mounted(){
+			//钩子函数
+			//在模板渲染成html后调用，通常是初始化页面完成后，再对html的dom节点进行一些需要的操作。
 			this.showLogin = true;
 			if (!this.adminInfo.id) {
-    			this.getAdminData()
+				console.log("执行了"+getAdminData+"函数");
+    			this.getAdminData();
     		}
 		},
 		computed: {
